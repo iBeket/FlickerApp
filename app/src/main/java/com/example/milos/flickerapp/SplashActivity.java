38f7a6c4a70cda0f349aa25ce44f21831b3e36a6
iBeket <milos.milosavljevic21@gmail.com>
2017-08-21 14:42:14 +0200

    animation for splash screen is added

--- app/src/main/java/com/example/milos/flickerapp/SplashActivity.java
+++ app/src/main/java/com/example/milos/flickerapp/SplashActivity.java
@@ -9,7 +9,11 @@ import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
 
 /**
  * Created by Milos on 08-Aug-17.
@@ -21,6 +25,11 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
+
+        LinearLayout mainFrame = ((LinearLayout) findViewById(R.id.splash));
+        Animation hyperspaceJumpAnimation = AnimationUtils.loadAnimation(this,
+                R.anim.hyperspace_jump);
+        mainFrame.startAnimation(hyperspaceJumpAnimation);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);

a88eaf0ec24953282d06ee8947989c7e6e4e8dbe
iBeket <milos.milosavljevic21@gmail.com>
2017-10-09 17:53:04 +0200

    you can now choose if you want to choose photo from gallery or just take photo by your self

--- app/src/main/java/com/example/milos/flickerapp/UploadImageActivity.java
+++ app/src/main/java/com/example/milos/flickerapp/UploadImageActivity.java
@@ -1,17 +1,26 @@
 package com.example.milos.flickerapp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ContextThemeWrapper;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
-import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+
 
 /**
  * Created by Milos on 09-Oct-17.
@@ -19,7 +28,16 @@ import java.lang.reflect.Method;
 
 public class UploadImageActivity extends AppCompatActivity {
 
-    ImageButton uploadButton;
+    private static final int SELECT_PHOTO = 100;
+    private static final int CAMERA_REQUEST = 1992;
+
+    ImageButton uploadButtonOptions;
+    ImageView uploadImage;
+    EditText uploadTitle;
+    EditText uploadDescription;
+    EditText uploadTags;
+    Button uploadButton;
+
     Context context;
 
     @Override
@@ -33,9 +51,14 @@ public class UploadImageActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        uploadButton = (ImageButton) findViewById(R.id.button_upload);
+        uploadImage = (ImageView) findViewById(R.id.image_upload);
+        uploadTitle = (EditText) findViewById(R.id.title_upload);
+        uploadDescription = (EditText) findViewById(R.id.description_upload);
+        uploadTags = (EditText) findViewById(R.id.tags_upload);
+        uploadButton = (Button) findViewById(R.id.upload_image_button);
+        uploadButtonOptions = (ImageButton) findViewById(R.id.button_upload);
 
-        uploadButton.setOnClickListener(new View.OnClickListener() {
+        uploadButtonOptions.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -62,9 +85,13 @@ public class UploadImageActivity extends AppCompatActivity {
                 popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     public boolean onMenuItemClick(MenuItem item) {
                         if (item.getTitle().equals(context.getString(R.string.upload_camera))) {
+                            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(cameraIntent, CAMERA_REQUEST);
 
                         } else if (item.getTitle().equals(context.getString(R.string.upload_gallery))) {
-
+                            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                            photoPickerIntent.setType("image/*");
+                            startActivityForResult(photoPickerIntent, SELECT_PHOTO);
                         }
                         return true;
                     }
@@ -73,6 +100,33 @@ public class UploadImageActivity extends AppCompatActivity {
                 popup.show();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch (requestCode) {
+            case SELECT_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    InputStream imageStream = null;
+                    try {
+                        imageStream = getContentResolver().openInputStream(selectedImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    Bitmap imageGallery = BitmapFactory.decodeStream(imageStream);
+                    uploadImage.setImageBitmap(imageGallery);
+                }
+            case CAMERA_REQUEST:
+                if (resultCode == RESULT_OK && requestCode == CAMERA_REQUEST) {
+
+                    Bundle extras = imageReturnedIntent.getExtras();
+                    Bitmap bmp = (Bitmap) extras.get("data");
+                    uploadImage.setImageBitmap(bmp);
+                }
+        }
     }
 
     @Override
